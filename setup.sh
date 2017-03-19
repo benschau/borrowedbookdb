687883f47b0ee5c22f890c0d827dfe6d5b1de7be
benschau <bchau3440@gmail.com>
2017-03-19 11:53:52 -0400

    general idea for program setup

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# setup.sh - setup postgresql environment.
+#   1. Check if the postgresql database and table have been created. 
+#   2. If yes, do nothing. If only the database, create the table. 
+#   3. Create the database.
+#   4. Give the database the correct access permissions.
+# 5.? Set environmental variables for... title of database? title of table?
+
